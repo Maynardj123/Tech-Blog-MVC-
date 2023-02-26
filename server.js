d5c0062f8447ca0219b6edb914f740c5c52b44e9
jacob m <jacobamaynard@gmail.com>
2023-02-26 13:03:01 -0600

    still figuring out error

--- server.js
+++ server.js
@@ -6,12 +6,27 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-// const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
